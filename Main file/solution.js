1918f3e1224469d83b1b8de1495221b522cf027a
Sergius Justus Chesami Nyah <74214119+Sergius-Nyah@users.noreply.github.com>
2023-09-14 18:14:34 +0100

    Moved directories

--- Main file/solution.js
+++ Main file/solution.js
@@ -25,9 +25,10 @@ inquirer
     } else {
       // Something else went wrong
     }
-  });
+  }); 
 /* 
 1. Use the inquirer npm package to get user input.
 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
 3. Create a txt file to save the user input using the native fs node module.
 */
+ 
