12efed4e31ea71c5db99aa9546710d2a465245b2
Lyubomir <127299159+LyubomirT@users.noreply.github.com>
2023-10-10 11:08:20 -0600

    Update index.js

--- Main file/index.js
+++ Main file/index.js
@@ -2,38 +2,65 @@ import inquirer from "inquirer";
 import qr from "qr-image";
 import fs from "fs";
 
-inquirer
-  .prompt([
-    {
-        message: "Type in your URL: ",  // Gets user input (URL, in this case) using the inquirer package, 
-        name: "URL"
-    }
-  ])
-  .then((answers) => {
-    // Use user feedback for... whatever!!
-    const url= answers.URL;
-    var qr_svg = qr.image(url);
-    qr_svg.pipe(fs.createWriteStream('qr_image.png'));
+// Define a regular expression to validate the user input.
+const urlRegex = /^(?:http(s)?:\/\/)?[a-zA-Z0-9-\.]+\.[a-zA-Z]{2,}(?::[0-9]{1,5})?(\/.*)?$/;
 
-    fs.writeFile("URL.txt", url, (err)=>  // Creates a text file to store the generated qr-code.
+// Define an asynchronous function to generate the QR code.
+async function generateQRCode(url) {
+  // Validate the user input.
+  if (!urlRegex.test(url)) {
+    throw new Error("Invalid URL");
+  }
+
+  // Generate the QR code image.
+  const qrCode = qr.image(url);
+
+  // Write the QR code image to a file.
+  await fs.promises.writeFile("qr_image.png", qrCode);
+}
+
+// Define an asynchronous function to save the URL to a file.
+async function saveURL(url) {
+  // Write the URL to a file.
+  await fs.promises.writeFile("URL.txt", url);
+}
+
+// Define the main function.
+async function main() {
+  // Get the user input.
+  const { URL } = await inquirer.prompt([
     {
-        if (err) throw err;
-        console.log("The file has been saved!");
-    });
-  
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+      message: "Type in your URL: ",
+      name: "URL",
+      validate: (input) => {
+        if (!urlRegex.test(input)) {
+          return "Invalid URL";
+        }
+
+        return true;
+      },
+    },
+  ]);
+
+  // Generate the QR code.
+  await generateQRCode(URL);
+
+  // Save the URL to a file.
+  await saveURL(URL);
+
+  // Log a success message.
+  console.log("The QR code and URL have been generated successfully!");
+}
+
+// Call the main function.
+main().catch((error) => {
+  // Log the error.
+  console.error(error);
+});
 
 /* 
-
-
 1. Use the inquirer npm package to get user input.
 2. Use the qr-image npm package to turn the user entered URL into a QR code image.
 3. Create a txt file to save the user input using the native fs node module.
 */
+
