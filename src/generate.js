184c70228459ee910a8a74f6bf6b4a617da0043c
Sergius Justus Chesami Nyah <74214119+Sergius-Nyah@users.noreply.github.com>
2023-12-02 20:39:24 +0100

    Modify the /generate-qr endpoint to return an HTML response that includes the QR Code image, instead on a JSON

--- src/generate.js
+++ src/generate.js
@@ -1,18 +1,31 @@
-import express from "express";
-import qr from "qr-image";
+import express from "express"; // Imports the express package, which would serve as our server
+import qr from "qr-image"; // Imports the qr-image package, which would help us generate the QR code
 
-const app = express();
-const port = 3000;
+const app = express(); // Creates an instance of the express server
+const port = 3000; // Sets the port to 3000
 
-app.use(express.json());
+app.use(express.json()); // Middleware to the Express application that parses incoming requests with JSON payloads,
+// hence making the data available on the req.body
 
-app.post("/generate-qr", (req, res) => {
-  const { url } = req.body;
+app.post("/generate-qr", (req, res) => { // Creates a POST route on the /generate-qr endpoint
+  const { url } = req.body; 
 
   const qr_svg = qr.imageSync(url, { type: "svg" });
   const qrCode = qr_svg.toString();
 
-  res.json({ qrCode });
+  const htmlResponse = `
+  <html>
+    <head>
+      <title>Generated QR Code</title>
+    </head>
+    <body>
+      <h1>Generated QR Code</h1>
+      <img src="data:image/svg+xml;base64,${Buffer.from(qrCode).toString('base64')}" alt="QR Code">
+    </body>
+  </html>
+`;
+
+res.send(htmlResponse);
 });
 
 app.listen(port, () => {
