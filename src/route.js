faee6a1a275a7c5103dd5caaf47e41864de64c26
Sergius Justus Chesami Nyah <74214119+Sergius-Nyah@users.noreply.github.com>
2023-12-06 20:31:31 +0100

    DLD link

--- src/route.js
+++ src/route.js
@@ -1,28 +1,28 @@
 const urlInput = document.getElementById('url-input');
 const convertButton = document.getElementById('convert-button');
-const successMessage = document.getElementById('success-message');
-const errorMessage = document.getElementById('error-message');
-const qrCodeDownloadLink = document.getElementById('qr-code-download');
 const qrCodeImage = document.getElementById('qr-code-image');
 
-convertButton.addEventListener('click', () => {
-  const url = urlInput.value; 
+  convertButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    const url = urlInput.value;
 
-  fetch('/generate-qr', { 
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },   
-    body: JSON.stringify({url})
-  })
-  .then(response => response.json())
-  .then(data => {
-    if (data && data.path) {
-      qrCodeImage.src = data.path; 
-    } else {
-      console.error('Invalid URL, Enter a valid URL');
-    }
-  })
-  .catch(error => console.error('Error:', error)); 
-  
-});
+    fetch('/generate-qr-code', {
+      method: 'POST', 
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ url })
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data && data.path) {
+          qrCodeImage.src = data.path;
+          document.getElementById('qr-code-download-link').href = data.path;
+          document.getElementById('success-message').style.display = 'block';
+        } else {
+          console.error('Invalid URL, Enter a valid URL');
+          document.getElementById('error-message').style.display = 'block';
+        }
+      })
+      .catch(error => console.error('Error:', error));
+  });
