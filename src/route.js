cf2e21f7afeb5b003fd3b2d7649627fc624175aa
Sergius Justus Chesami Nyah <74214119+Sergius-Nyah@users.noreply.github.com>
2023-12-06 19:31:14 +0100

    Removed logging and display changes

--- src/route.js
+++ src/route.js
@@ -8,30 +8,21 @@ const qrCodeImage = document.getElementById('qr-code-image');
 convertButton.addEventListener('click', () => {
   const url = urlInput.value; 
 
-  fetch('http://localhost:3000/generate-qr', { 
+  fetch('/generate-qr', { 
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },   
     body: JSON.stringify({url})
   })
-  .then(response => {
-    console.log('Response status:', response.status); // Log the response status
-    return response.json();
-  })
+  .then(response => response.json())
   .then(data => {
-    console.log('Response body:', data); // Log the response body
-    if (data.path) {
+    if (data && data.path) {
       qrCodeImage.src = data.path; 
-      successMessage.style.display = 'block';
-      errorMessage.style.display = 'none';
     } else {
-      throw new Error('Invalid URL, Enter a valid URL');
+      console.error('Invalid URL, Enter a valid URL');
     }
   })
-  .catch(error => {
-    console.error('Error:', error);
-    successMessage.style.display = 'none';
-    errorMessage.style.display = 'block';
-  });
+  .catch(error => console.error('Error:', error)); 
+  
 });
