890d0331a4b9c598b6ac23ede68f23a8f6c084ae
Sergius Justus Chesami Nyah <74214119+Sergius-Nyah@users.noreply.github.com>
2023-12-06 22:32:58 +0100

    Render Code directly to frontend

--- src/App.js
+++ src/App.js
@@ -2,46 +2,34 @@ import express from 'express';
 import qr from 'qr-image';
 import bodyParser from 'body-parser';
 import { dirname } from "path";
-import path from "path";
 import { fileURLToPath } from "url";
 import fs from 'fs';
-import cors from 'cors';
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import express from 'express';
-import qr from 'qr-image';
-import bodyParser from 'body-parser';
-import fs from 'fs';
+import cors from 'cors'; //Cross-Origin Resource Sharing (CORS) is a mechanism that uses additional HTTP headers to tell browsers to give a web application running at one origin, access to selected resources from a different origin.
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-const port = 3000;
+const port = 3000; 
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public')); //Render static files
 
-app.post('/generate-qr-code', (req, res) => {
-  const url = req.body.url;
+app.get('/generate-qr-code', (req, res) => {
+  const url = req.body.url;// Get URL from Query parameter
 
   if (!url) {
     return res.status(400).json({ error: 'Invalid URL' });
   }
 
-  const qrPng = qr.image(url, { type: 'png' });
+  const qrPng = qr.image(url, { type: 'png' }); // Generate QR code
+  res.type('png');
+  qrPng.pipe(res);
 
   qrPng.on('error', (error) => { 
     console.error(error);
     return res.status(500).json({ error: 'Failed to generate QR code' });
-  });
-
-  const qrCodePath = 'public/qr-code.png';
-  const qrCodeStream = qrPng.pipe(fs.createWriteStream(qrCodePath));
-
-  qrCodeStream.on('finish', () => {
-    res.json({ path: `/${qrCodePath}` });
   });
 });
 
