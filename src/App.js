e892a19e1e347e5b7189a6238d709ff4f7de1283
Sergius Justus Chesami Nyah <74214119+Sergius-Nyah@users.noreply.github.com>
2023-12-06 20:30:34 +0100

    Cors doc

--- src/App.js
+++ src/App.js
@@ -5,34 +5,44 @@ import { dirname } from "path";
 import path from "path";
 import { fileURLToPath } from "url";
 import fs from 'fs';
+import cors from 'cors';
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import express from 'express';
+import qr from 'qr-image';
+import bodyParser from 'body-parser';
+import fs from 'fs';
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static('public')); //Render static files
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*'); // Allow Cross Origin Requests
-  next();
-}); 
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
-}); 
+app.post('/generate-qr-code', (req, res) => {
+  const url = req.body.url;
 
-app.post('/generate-qr', (req, res) => {
-  const url = req.body.url; 
+  if (!url) {
+    return res.status(400).json({ error: 'Invalid URL' });
+  }
 
   const qrPng = qr.image(url, { type: 'png' });
-  qrPng.pipe(fs.createWriteStream('public/qr-code.png'));
 
-  res.json({ path: '/qr-code.png' });
-});
+  qrPng.on('error', (error) => { 
+    console.error(error);
+    return res.status(500).json({ error: 'Failed to generate QR code' });
+  });
 
-app.get('/qr-code.png', (req, res) => {
-  res.sendFile(__dirname + '/qr-code.png');
+  const qrCodePath = 'public/qr-code.png';
+  const qrCodeStream = qrPng.pipe(fs.createWriteStream(qrCodePath));
+
+  qrCodeStream.on('finish', () => {
+    res.json({ path: `/${qrCodePath}` });
+  });
 });
 
 app.listen(port, () => {
