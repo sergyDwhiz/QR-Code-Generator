876a24bbdf20aa205d769e95462220f9fbf4a032
Sergius Justus Chesami Nyah <74214119+Sergius-Nyah@users.noreply.github.com>
2023-12-07 21:59:06 +0100

    Sends response as encoded text

--- src/route.js
+++ src/route.js
@@ -1,28 +1,43 @@
-const urlInput = document.getElementById('url-input');
-const convertButton = document.getElementById('convert-button');
-const qrCodeImage = document.getElementById('qr-code-image');
+import { response } from "express";
 
-  convertButton.addEventListener('click', (event) => {
-    event.preventDefault();
-    const url = urlInput.value;
+const urlForm = document.getElementById('urlForm');
+const urlInput = document.getElementById('urlInput');
+const codeContainer= document.getElementById('qrCodeContainer');
 
-    fetch('/generate-qr-code', {
-      method: 'POST', 
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ url })
-    })
-      .then(response => response.json())
-      .then(data => {
-        if (data && data.path) {
-          qrCodeImage.src = data.path;
-          document.getElementById('qr-code-download-link').href = data.path;
-          document.getElementById('success-message').style.display = 'block';
-        } else {
-          console.error('Invalid URL, Enter a valid URL');
-          document.getElementById('error-message').style.display = 'block';
-        }
-      })
-      .catch(error => console.error('Error:', error));
+  // convertButton.addEventListener('click', (event) => {
+  //   event.preventDefault();
+  //   const url = urlInput.value;
+
+  //   fetch('/generate-qr-code', {
+  //     method: 'POST', 
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     },
+  //     body: JSON.stringify({ url })
+  //   })
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       if (data && data.path) {
+  //         qrCodeImage.src = data.path;
+  //         document.getElementById('qr-code-download-link').href = data.path;
+  //         document.getElementById('success-message').style.display = 'block';
+  //       } else {
+  //         console.error('Invalid URL, Enter a valid URL');
+  //         document.getElementById('error-message').style.display = 'block';
+  //       }
+  //     })
+  //     .catch(error => console.error('Error:', error));
+  // });
+urlForm.addEventListener('submit', function(event)
+{
+  event.preventDefault();
+  const url= urlInput.value;
+  fetch(`/generate-qr?url=${encodeURIComponent(url)}`)
+  .then(response=> response.text())
+  .then(html=> {
+codeContainer.innerHTML= html;
+  })
+  .catch(error=> {
+    console.error('Error generating QR code:', error);
   });
+});
